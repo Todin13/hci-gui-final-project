f81897459460d4acd6fda22ca998231b4fc28f85
Thomas ODIN <thomasodin13@gmail.com>
2024-12-27 00:01:44 +0100

    Fix the mouseEvent function and setting the color of the piece in function of player number at first may transfrom into a function after

--- HGP_Group_12_Project/code/board.py
+++ HGP_Group_12_Project/code/board.py
@@ -15,6 +15,8 @@ class Board(QFrame):  # base the board on a QFrame widget
     boardHeight = 7  #board is 7 squares high
     timerSpeed = 1000  # the timer updates every 1 second
     counter = 10  # the number the counter will count down from
+    player_turn = 0 # player turn 0 for nobody game not started
+    isStarted = False  # game is not currently started
 
     def __init__(self, parent):
         super().__init__(parent)
@@ -26,13 +28,11 @@ class Board(QFrame):  # base the board on a QFrame widget
         self.timer.timeout.connect(
             self.timerEvent
         )  # connect timeout signal to timerEvent method
-        self.isStarted = False  # game is not currently started
-        self.start()  # start the game which will start the timer
-
+        # self.start()  # start the game which will start the timer we will use a button
         self.boardArray = (
             [[Piece(0, r, c) for c in range(7)] for r in range(7)]
         )  # create a 2d int/Piece array to store the state of the game
-        self.printBoardArray()
+        self.printBoardArray() # for debug
 
     def printBoardArray(self):
         """prints the boardArray in an attractive way"""
@@ -63,6 +63,10 @@ class Board(QFrame):  # base the board on a QFrame widget
         self.resetGame()  # reset the game
         self.timer.start(self.timerSpeed)  # start the timer with the correct speed
         print("start () - timer is started")
+        self.player_turn = 2
+        self.print_player_turn()
+        self.play_game()
+        
 
     def timerEvent(self):
         """this event is automatically called when the timer is updated. based on the timerSpeed variable"""
@@ -81,12 +85,22 @@ class Board(QFrame):  # base the board on a QFrame widget
 
     def mousePressEvent(self, event):
         """this event is automatically called when the mouse is pressed"""
-        clickLoc = (
-            "click location [" + str(event.x()) + "," + str(event.y()) + "]"
-        )  # the location where a mouse click was registered
-        print("mousePressEvent() - " + clickLoc)
-        # TODO you could call some game logic here
-        self.clickLocationSignal.emit(clickLoc)
+        # Convert the mouse click position to a row and column
+        col = int(event.position().x() // self.squareWidth())
+        row = int(event.position().y() // self.squareHeight())
+
+        # Ensure the click is within the board boundaries
+        if 0 <= row < self.boardHeight and 0 <= col < self.boardWidth:
+            # Toggle the state of the piece at the clicked position
+            piece = self.boardArray[row][col]
+            piece.change_state(1)
+            self.printBoardArray() # to debug
+            
+            # Log the click and update the board
+            clickLoc = f"({row}, {col})"
+            print("mousePressEvent() -  Location :" + clickLoc)
+            self.clickLocationSignal.emit(clickLoc) # prof put it but i don't like it need moddification
+            self.update()  # Redraw the board
 
     def resetGame(self):
         """clears pieces from the board"""
@@ -118,5 +132,23 @@ class Board(QFrame):  # base the board on a QFrame widget
                 # TODO choose your color and set the painter brush to the correct color
                 radius = (self.squareWidth() - 2) / 2
                 center = QPoint(int(radius), int(radius))
+
+                # Set color based on player_turn and piece state
+                if self.player_turn == 0:  # Player turn 0: all circles white
+                    painter.setPen(QColor(255, 255, 255))  # Black outline
+                    painter.setBrush(QBrush(QColor(255, 255, 255)))  # White
+                elif self.player_turn == 1:  # Player turn 1: outline in black
+                    painter.setPen(QColor(0, 0, 0))  # Black outline
+                elif self.player_turn == 2:  # Player turn 2: fill circle in black
+                    painter.setBrush(QBrush(QColor(0, 0, 0)))  # Black fill
+
+
                 painter.drawEllipse(center, int(radius), int(radius))
                 painter.restore()
+
+    def print_player_turn(self):
+        """
+        Print for debug the player turn
+        """
+        color = "black" if self.player_turn == 2 else "white" 
+        print(f"player {self.player_turn} ({color}) turn")
